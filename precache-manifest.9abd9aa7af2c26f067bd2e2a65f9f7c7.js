079d6641b0377447f885e3ac3713fa249eb6994b
Vincent Kay <vincekay10@gmail.com>
2019-10-29 17:51:29 -0700

    Updates

---
+++ precache-manifest.9abd9aa7af2c26f067bd2e2a65f9f7c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a0a065feba46eefb696e81d4a7f0b3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "82f24ff3e933febae7c4",
+    "url": "/portfolio/static/css/main.2f4705df.chunk.css"
+  },
+  {
+    "revision": "010854d744fd44fc6ecc",
+    "url": "/portfolio/static/js/2.31d366b0.chunk.js"
+  },
+  {
+    "revision": "82f24ff3e933febae7c4",
+    "url": "/portfolio/static/js/main.a2765af7.chunk.js"
+  },
+  {
+    "revision": "feae9fcf938d74b574b1",
+    "url": "/portfolio/static/js/runtime-main.d0fd4f28.js"
+  }
+]);
